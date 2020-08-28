e3f560c4f3e3815e054fd5acb2ec7d6cb7b6ab12
pierrewebdev <patrick.pierre000@gmail.com>
2020-08-27 23:09:37 -0400

    I get the [point of rails url helpers now

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show]
+  get "posts/:id", to: "posts#show", as: "showstopper"
+  get "posts", to: "posts#index"
 end
